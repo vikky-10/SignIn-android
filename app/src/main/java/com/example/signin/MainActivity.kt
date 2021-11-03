ec0d039e2b576831008745bb9d187102bcfd7df1
vikky singh <vikkyhp0007@gmail.com>
2021-11-03 15:13:10 +0530

    add snackbar

--- app/src/main/java/com/example/signin/MainActivity.kt
+++ app/src/main/java/com/example/signin/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.signin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var llmain: LinearLayout = findViewById(R.id.main);
+        var signIn: Button = findViewById(R.id.Sing_in) as Button;
+        signIn.setOnClickListener {
+            val actionSnackbar = Snackbar.make(llmain,"Done successfully", Snackbar.LENGTH_LONG).setAction("Ok") {
+                // Call action functions here
+            }.show()
+             
+        }
     }
 }
